a994ac83a254d1f672d12069d06f5a0804e70dbd
Dener Matos <denerdonizetti@hotmail.com>
2023-01-04 00:09:10 -0300

    Feat:Implement options

--- src/__init__.py
+++ src/__init__.py
@@ -12,6 +12,7 @@ from src.controllers.empresa_controller import *
 from src.controllers.home_controller import *
 from src.controllers.registration_user_controller import *
 from src.controllers.plataform_controller import *
+from src.controllers.reports_controller import *
 
 
 
--- src/controllers/reports_controller.py
+++ src/controllers/reports_controller.py
@@ -2,12 +2,12 @@ from flask import render_template, redirect
 from src import app
 from src.services.validation_register_user_services import AccessControl as ACL
 
-@app.route('/plataform', methods = ['GET', 'POST'])
-def plataform_access():
+@app.route('/reports', methods = ['GET', 'POST'])
+def views_reports():
     # isPermit = ACL.avaliable()
     # if not isPermit:
     #     return redirect('/')
 
     
-    return render_template("plataform.html");
+    return render_template("reports.html");
 
