81839aaba3a8cd4e3215b7d351e43294bd8c1441
Dener Matos <denerdonizetti@hotmail.com>
2023-01-14 07:53:05 -0300

    Feat: Add settings of groups

--- src/controllers/settings_controller.py
+++ src/controllers/settings_controller.py
@@ -15,3 +15,11 @@ def menu_settings():
 @app.route('/settings-church', methods = ['GET', 'POST'])
 def settigns_church():
     return render_template("settings-church.html")
+
+@app.route('/settings-groups', methods = ['GET', 'POST'])
+def settings_groups(): 
+    return render_template("settings-groups.html")
+
+@app.route('/list-groups', methods = ['GET', 'POST'])
+def list_groups():
+    return render_template('list-groups.html')
