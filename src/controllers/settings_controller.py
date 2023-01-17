bddca1b53f8b303c68df1a421703d01c38b9b1d6
Dener Matos <denerdonizetti@hotmail.com>
2023-01-16 23:19:58 -0300

    Feat: menu settings cults

--- src/controllers/settings_controller.py
+++ src/controllers/settings_controller.py
@@ -16,10 +16,20 @@ def menu_settings():
 def settigns_church():
     return render_template("settings-church.html")
 
+
 @app.route('/settings-groups', methods = ['GET', 'POST'])
 def settings_groups(): 
     return render_template("settings-groups.html")
 
+
 @app.route('/list-groups', methods = ['GET', 'POST'])
 def list_groups():
     return render_template('list-groups.html')
+
+@app.route('/settings-cults', methods = ['GET', 'POST'])
+def settings_cults(): 
+    return render_template('settings-cults.html')
+
+@app.route('/list-cults', methods = ['GET', 'POST'])
+def list_cults(): 
+    return render_template('list-cults.html')
